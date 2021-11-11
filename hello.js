1d80b49f46148ad6d1f2c1022de45e051e1dcd87
Fil2780 <filgirma.hail@gmail.com>
2021-11-11 03:47:21 +0000

    declaring a function

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
 }
-sayHello('burt'); 
+const greeting = returnSayHello('John');
+console.log(greeting);
