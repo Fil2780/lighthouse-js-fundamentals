a931343fc377db9a7f9b9ee12d793757da4aa6db
Fil2780 <filgirma.hail@gmail.com>
2021-11-14 04:50:34 +0000

    example addition on making a loop

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while  (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+for (let j = 0; j <= (ingredients.length - 1); j++) {
+  console.log(ingredients[j]);
+}
+for (let k = (ingredients.length-1); k >= 0; k--) {
+  console.log(ingredients[k]);
+} 
